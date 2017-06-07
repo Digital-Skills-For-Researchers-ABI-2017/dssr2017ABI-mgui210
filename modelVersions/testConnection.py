87ec46a8c83cd8f08d93ef1f2454c5e31b51132d
Megan Guidry <mgui210@aucklanduni.ac.nz>
2017-06-07 17:02:27 +1200

    I have made a few additions to the test file. I am trying to import a parameter from InterfaceTest.py into this file but it is not currently working.  I am getting the error 'AttributeError: 'module' object has no attribute 'dataFile''

--- modelVersions/testConnection.py
+++ modelVersions/testConnection.py
@@ -1,1 +1,8 @@
-import ..\InterfaceTest
+import sys 
+import os
+sys.path.append(os.path.abspath("../"))
+import InterfaceTest
+print("IMPORT SUCCESS!")
+
+dataFile = InterfaceTest.dataFile
+print("Testoutput " + dataFile)
