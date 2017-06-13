7430941890e95772357e481511b0210f79383b9a
Megan Guidry <mgui210@aucklanduni.ac.nz>
2017-06-14 09:35:15 +1200

    I have added a few 'setup' lines to my test doc

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+import inspect
+import sys
+import InterfaceTest
+
+class CheckInput(unittest.TestCase):
+
+    # Check the input can be changed into an integer (is a single number)
+    def test_integer(self):
+        self
